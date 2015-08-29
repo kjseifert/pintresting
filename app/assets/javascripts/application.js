9a81f73b991565c17426b389fd80f124b85cd5af
Kevin Seifert <kevin.j.seifert@gmail.com>
2015-08-29 15:53:41 +0000

    jquery mason (not working...yet)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
+//= require jquery.turbolinks
+//= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require_tree .
